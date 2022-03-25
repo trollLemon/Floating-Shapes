ae5010a27f5aabd0f3fe2ce54670fa2d9be2e96d
haydn <haydnt@zohomail.com>
2022-03-25 13:03:01 -0700

    added a rng for the direction of shapes

--- src/random_data.rs
+++ src/random_data.rs
@@ -3,8 +3,18 @@ use sdl2::pixels::Color;
 
 pub fn rng(limits: (i32, i32)) -> (i32, i32) {
     (
+        rand::thread_rng().gen_range(0..limits.0),
         rand::thread_rng().gen_range(0..limits.1),
-        rand::thread_rng().gen_range(0..limits.1),
+    )
+}
+
+/*
+    Similar to the other rng function but for direction, and one or both entries in the tupal can be negative
+*/
+pub fn rng_direction(limits: (i32, i32)) -> (i32, i32) {
+    (
+        rand::thread_rng().gen_range(limits.0..limits.1),
+        rand::thread_rng().gen_range(limits.0..limits.1),
     )
 }
 
@@ -25,7 +35,7 @@ mod tests {
 
     use sdl2::pixels::Color;
 
-    use crate::random_data::{rng, rng_but_color};
+    use crate::random_data::{rng, rng_but_color, rng_direction};
 
     #[test]
     fn test_rng() {
@@ -33,23 +43,33 @@ mod tests {
 
         let random_numbers = rng(limit);
 
-        println!("{},{}", limit.0, limit.1);
-
-        assert_eq!(random_numbers.0 >= 0 && random_numbers.1 <= 10, true);
+        assert!(random_numbers.0 >= 0 && random_numbers.1 <= 10);
 
         //test this several more times
 
         let random_numbers = rng(limit);
 
-        assert_eq!(random_numbers.0 >= 0 && random_numbers.1 <= 10, true);
+        assert!(random_numbers.0 >= 0 && random_numbers.1 <= 10);
 
         let random_numbers = rng(limit);
 
-        assert_eq!(random_numbers.0 >= 0 && random_numbers.1 <= 10, true);
+        assert!(random_numbers.0 >= 0 && random_numbers.1 <= 10);
 
         let random_numbers = rng(limit);
 
-        assert_eq!(random_numbers.0 >= 0 && random_numbers.1 <= 10, true);
+        assert!(random_numbers.0 >= 0 && random_numbers.1 <= 10);
+    }
+
+    #[test]
+    fn test_rng_direction() {
+        let limits = (-3, 3);
+
+        let random_dxdy = rng_direction(limits);
+
+        assert!(
+            (random_dxdy.0 <= 3 && random_dxdy.0 >= -3)
+                && (random_dxdy.1 <= 3 && random_dxdy.1 >= -3)
+        );
     }
 
     #[test]
