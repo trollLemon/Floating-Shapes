d78ee788d04ac4577afcdcb9d6190358e1007c13
haydn <haydnt@zohomail.com>
2022-03-25 13:02:16 -0700

    removed unneeded use

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod random_data;
 mod shape;
-//mod random_data;
+
 mod app;
 use app::run;
 use std::io::Error;
 pub fn main() -> Result<(), Error> {
     run().unwrap_or_else(|error| {
         println!("{}", error);
-    }); //start the app
+    }); //start the app, output an error if it crashes
     Ok(())
 }
