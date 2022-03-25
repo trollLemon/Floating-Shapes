20a78fe56c4bc71f455f99de9218a082ab0081e2
haydn <haydnt@zohomail.com>
2022-03-24 17:24:37 -0700

    changed code to work with sdl2, it will call a run function which is in app.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use druid::{AppLauncher, PlatformError, WindowDesc};
-
-mod draw_ui;
+mod random_data;
 mod shape;
-use crate::draw_ui::ui::ui_builder;
-fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder);
-    let data = 0_u32;
-    AppLauncher::with_window(main_window)
-        .use_simple_logger()
-        .launch(data)
+//mod random_data;
+mod app;
+use app::run;
+use std::io::Error;
+pub fn main() -> Result<(), Error> {
+    run().unwrap_or_else(|error| {
+        println!("{}", error);
+    }); //start the app
+    Ok(())
 }
