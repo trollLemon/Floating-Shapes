ec68e7cba5d36efca1ce382d7c087f028a63b4eb
haydn <haydnt@zohomail.com>
2022-03-28 11:26:19 -0700

    Demo shape, and added collision detector for the demo shape.

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use std::io::Error;
 
+use crate::collisions::collision_handelers::collision_handeler;
 use crate::shape::shapes::{self, Shape};
 
 use sdl2::event::Event;
@@ -21,7 +22,7 @@ pub fn run() -> Result<(), Error> {
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("Floating Shapes", 1000, 1000)
+        .window("Floating Shapes", 500, 500)
         .position_centered()
         .build()
         .unwrap();
@@ -30,7 +31,7 @@ pub fn run() -> Result<(), Error> {
 
     canvas.set_draw_color(Color::RGB(255, 255, 255));
 
-    let first_shape: shapes::Square = shapes::Square::new_shape((1000, 1000), (-5, 5));
+    let first_shape: shapes::Square = shapes::Square::new_shape((500, 500), (-3, 3));
 
     square_objects.push(first_shape);
 
@@ -61,6 +62,16 @@ pub fn run() -> Result<(), Error> {
 
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
+
+        let curr_location = square_objects[0].get_location();
+        let curr_direction = square_objects[0].get_direction();
+
+        square_objects[0].change_direction(collision_handeler(
+            curr_direction,
+            curr_location,
+            (500, 500),
+        ));
+
         square_objects[0].update_position();
 
         let rect = rect::Rect::new(
