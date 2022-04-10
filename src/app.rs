462e19f322e8b2753fc42ce4f1929411a4466aa9
haydn <haydnt@zohomail.com>
2022-04-09 21:10:47 -0700

    can spawn circles ans squares, resize them, and delete them in order, size of square and circle have also been reduced, and collision detection is run with the shapes collision functions

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
 use std::io::Error;
 
-use crate::collisions::collision_handelers::collision_handeler;
+use crate::circle;
 
-use crate::shape::shapes::{self, Shape};
+use crate::circle::circle::Circle;
+use crate::shape::shapes::Shape;
+use crate::square::square::Square;
 
 use sdl2::event::Event;
 
@@ -19,30 +21,35 @@ use std::time::Duration;
 
 use rand::Rng;
 
+const DIMENSIONS: (u32, u32) = (1000, 1000);
+const BOUNDS: (i32, i32) = (1000, 1000);
+
 pub fn run() -> Result<(), Error> {
     /*
         a bunch of vectors that store each shape so we can keep track of thier position and do collisions
     */
-    let mut square_objects: Vec<shapes::Square> = vec![];
-    let mut circle_objects: Vec<shapes::Circle> = vec![];
-    let mut width_and_height: (i32, i32) = (100, 100);
-    const RADIUS: i32 = 50;
+    let mut square_objects: Vec<Square> = vec![];
+    let mut circle_objects: Vec<Circle> = vec![];
+    let mut delete_list: Vec<i32> = vec![]; //used to figure out wether to delete a square or a circle (0 = square, 1 = circle)
+
+    let mut width_and_height: (i32, i32) = (50, 50);
+    let mut radius = 25;
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("Floating Shapes", 1000, 1000)
+        .window("Floating Shapes", DIMENSIONS.0, DIMENSIONS.1)
         .position_centered()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
 
     canvas
         .filled_circle(100, 100, 100, Color::RED)
-        .expect(r#"oops"#);
-    //canvas.set_draw_color(Color::RGB(255, 255, 255));
+        .expect("error drawing");
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
 
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -58,8 +65,10 @@ pub fn run() -> Result<(), Error> {
 
                     if zero_or_one == 0 {
                         square_creator(width_and_height, &mut square_objects);
+                        delete_list.push(0);
                     } else {
-                        circle_creator(RADIUS, &mut circle_objects);
+                        circle_creator(radius, &mut circle_objects);
+                        delete_list.push(1);
                     }
                 }
 
@@ -67,7 +76,17 @@ pub fn run() -> Result<(), Error> {
                     keycode: Some(Keycode::D),
                     ..
                 } => {
-                    delete_a_shape(&mut square_objects);
+                    let id = delete_list.pop();
+
+                    match id {
+                        Some(0) => {
+                            delete_a_square(&mut square_objects);
+                        }
+                        Some(1) => {
+                            delete_a_circle(&mut circle_objects);
+                        }
+                        _ => {}
+                    }
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::C),
@@ -81,6 +100,7 @@ pub fn run() -> Result<(), Error> {
                     ..
                 } => {
                     increase_width(&mut width_and_height);
+                    increase_radius(&mut radius);
                 }
 
                 Event::KeyDown {
@@ -88,6 +108,7 @@ pub fn run() -> Result<(), Error> {
                     ..
                 } => {
                     increase_height(&mut width_and_height);
+                    increase_radius(&mut radius);
                 }
 
                 Event::KeyDown {
@@ -95,12 +116,14 @@ pub fn run() -> Result<(), Error> {
                     ..
                 } => {
                     decrease_width(&mut width_and_height);
+                    decrease_radius(&mut radius);
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::R),
                     ..
                 } => {
                     decrease_height(&mut width_and_height);
+                    decrease_radius(&mut radius);
                 }
 
                 Event::Quit { .. }
@@ -115,35 +138,23 @@ pub fn run() -> Result<(), Error> {
 
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
-        shape_handeler(
-            &mut square_objects,
-            &mut circle_objects,
-            &mut canvas,
-            width_and_height,
-            RADIUS,
-        );
+        shape_handeler(&mut square_objects, &mut circle_objects, &mut canvas);
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 144));
     }
     Ok(())
 }
 
 fn shape_handeler(
-    squares: &mut Vec<shapes::Square>,
-    circles: &mut Vec<shapes::Circle>,
+    squares: &mut Vec<Square>,
+    circles: &mut Vec<Circle>,
     canvas: &mut Canvas<Window>,
-    width_and_height: (i32, i32),
-    radius: i32,
 ) {
-    shape_updater(squares, circles, width_and_height, radius);
+    shape_updater(squares, circles);
     shape_drawer(squares, circles, canvas);
 }
 
-fn shape_drawer(
-    squares: &mut Vec<shapes::Square>,
-    circles: &mut Vec<shapes::Circle>,
-    canvas: &mut Canvas<Window>,
-) {
+fn shape_drawer(squares: &mut Vec<Square>, circles: &mut Vec<Circle>, canvas: &mut Canvas<Window>) {
     for shape in squares {
         let rect = rect::Rect::new(
             shape.get_location().0,
@@ -161,69 +172,53 @@ fn shape_drawer(
             .filled_circle(
                 shape.get_location().0.try_into().unwrap(),
                 shape.get_location().1.try_into().unwrap(),
-                shape.get_dimensions().1.try_into().unwrap(),
+                shape.get_radius().try_into().unwrap(),
                 shape.get_color(),
             )
-            .expect("oops");
+            .expect("error drawing");
     }
 }
 
-fn delete_a_shape(squares: &mut Vec<shapes::Square>) {
+fn delete_a_square(squares: &mut Vec<Square>) {
     squares.pop();
 }
 
-fn clear_canvas(squares: &mut Vec<shapes::Square>, circles: &mut Vec<shapes::Circle>) {
+fn delete_a_circle(circles: &mut Vec<Circle>) {
+    circles.pop();
+}
+
+fn clear_canvas(squares: &mut Vec<Square>, circles: &mut Vec<Circle>) {
     squares.clear();
     circles.clear();
 }
 
-fn shape_updater(
-    squares: &mut Vec<shapes::Square>,
-    circles: &mut Vec<shapes::Circle>,
-    width_and_height: (i32, i32),
-    radius: i32,
-) {
+fn shape_updater(squares: &mut Vec<Square>, circles: &mut Vec<Circle>) {
     for shape in squares {
-        let curr_location = shape.get_location();
-        let curr_direction = shape.get_direction();
-
-        shape.change_direction(collision_handeler(
-            curr_direction,
-            curr_location,
-            (1000, 1000),
-            width_and_height,
-        ));
-
+        shape.change_direction();
         shape.update_position();
     }
 
     for shape in circles {
-        let curr_location = shape.get_location();
-        let curr_direction = shape.get_direction();
-
-        shape.change_direction(collision_handeler(
-            curr_direction,
-            curr_location,
-            (1000, 1000),
-            (radius, radius),
-        ));
-
+        shape.change_direction();
         shape.update_position();
     }
 }
 
-fn square_creator(width_and_height: (i32, i32), squares: &mut Vec<shapes::Square>) {
-    let shape: shapes::Square = shapes::Square::new_shape(
-        (1000 - width_and_height.0, 1000 - width_and_height.1),
+fn square_creator(width_and_height: (i32, i32), squares: &mut Vec<Square>) {
+    let shape: Square = Square::new_shape(
+        (BOUNDS.0 - width_and_height.0, BOUNDS.1 - width_and_height.1),
         (-5, 5),
         width_and_height,
     );
     squares.push(shape);
 }
 
-fn circle_creator(radius: i32, circles: &mut Vec<shapes::Circle>) {
-    let shape: shapes::Circle =
-        shapes::Circle::new_shape((1000 - radius, 1000 - radius), (-5, 5), (radius, radius));
+fn circle_creator(radius: i32, circles: &mut Vec<circle::circle::Circle>) {
+    let shape: Circle = Circle::new_shape(
+        (BOUNDS.0 - radius, BOUNDS.1 - radius),
+        (-5, 5),
+        (radius, radius),
+    );
     circles.push(shape);
 }
 
@@ -242,4 +237,12 @@ fn decrease_height(dimensions: &mut (i32, i32)) {
     if dimensions.1 > 0 {
         dimensions.1 = dimensions.1 - 1;
     }
+}
+
+fn increase_radius(radius: &mut i32) {
+    *radius = *radius + 1;
+}
+
+fn decrease_radius(radius: &mut i32) {
+    *radius = *radius - 1;
 }
